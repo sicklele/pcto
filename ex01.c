c1f610409a87e7f5a98cebc4a89b7863412170a9
sicklele <128369812+sicklele@users.noreply.github.com>
2023-03-20 12:47:11 +0100

    Add files via upload

---
+++ ex01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int intero = 65;
+    char carattere = 'A';
+
+    printf ("intero come int: %d\n", intero);
+    printf ("intero come char: %c\n", intero);
+    printf ("carattere come int: %d\n", carattere);
+    printf ("carattere come char: %c\n", carattere);
+}
