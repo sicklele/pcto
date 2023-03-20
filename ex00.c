a5c58e497b62c828903747b4e304ede6944a6fb5
sicklele <128369812+sicklele@users.noreply.github.com>
2023-03-20 14:46:13 +0100

    Add files via upload

---
+++ ex00.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+float z = 3;
+char a = 'b';
+char c = 'C';
+int intero = 42;
+printf ("ho creato una variablie di tpo float %f, il primo carattere %c è guale al secondo carattere %c, ho creato una variabile di tipo int con valore %c\n", z, a, c, intero);
+
+}
