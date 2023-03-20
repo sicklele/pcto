4cdd99b159842b7816bc04cd7a6c0d1daabf32b2
sicklele <128369812+sicklele@users.noreply.github.com>
2023-03-20 15:32:32 +0100

    Add files via upload

---
+++ ex04.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+     int x;
+printf ("inserire la psropria età");
+scanf("%d", &x);
+if(x>18)
+{
+    printf ("sei maggiorenne");
+}
+
+else
+{
+    printf ("sei minorenne");
+}
+}
+   
